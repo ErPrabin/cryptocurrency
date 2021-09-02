cf7b6315b530a40b38f6f588dae4e1e70ac19a54
ErPrabin <prabinmaneeshrestha22@gmail.com>
2021-09-02 12:04:33 +0545

    login, register done

--- account/urls.py
+++ account/urls.py
@@ -6,4 +6,5 @@ app_name = 'account'
 urlpatterns = [
     path('login/', views.login,name='login'),
     path('register/', views.register,name='register'),
+    path('logout/', views.logout_view,name='logout'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,8 +1,13 @@
+from django.http import response
 from django.shortcuts import redirect, render
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout
+from django.contrib.auth import authenticate
+
 from django.contrib.auth.models import User, auth
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -13,6 +18,12 @@ def login(request):
         password = request.POST['password']
         print(username, password)
         user = authenticate(request, username=username, password=password)
+        print(user)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('/')
+        else:
+            return redirect('/account/login/')
 
     else:
         return render(request, 'login.html')
@@ -20,16 +31,19 @@ def login(request):
 
 def register(request):
     if request.method == 'POST':
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        email = request.POST['email']
-        password1 = request.POST['password1']
-        password2 = request.POST['password2']
-        username = request.POST['username']
-
-        user = User.objects.create_user(
-            username=username, first_name=first_name, last_name=last_name, email=email, password=password1)
-        user.save()
-        return redirect('/')
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            response = redirect('/')
+            # log the user in
+            return response
+        return redirect('/account/login/')
     else:
-        return render(request, 'register.html')
+        form = UserCreationForm()
+        return render(request, 'register.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('/account/login/')
+    # Redirect to a success page.
